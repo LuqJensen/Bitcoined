ad3f8b1724a71c02230aee3b0a8e582469ddbd58
LuqJensen <lucas__jensen@hotmail.com>
2018-10-21 01:12:14 +0200

    Basics

--- Bitcoined/Controllers/HomeController.cs
+++ Bitcoined/Controllers/HomeController.cs
@@ -1,37 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Bitcoined.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Bitcoined.Controllers
 {
     public class HomeController : Controller
     {
+        public const string IP = "localhost";
+        public const int PORT = 18443;
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public JObject InvokeMethod(string method, params object[] parameters)
         {
-            ViewData["Message"] = "Your application description page.";
+            Uri uri = new Uri($"http://{IP}:{PORT}");
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(uri);
+            webRequest.Credentials = new NetworkCredential()
+            {
+                Password = "guddav"
+            };
+            webRequest.ContentType = "application/json-rpc";
+            webRequest.Method = "POST";
 
-            return View();
+            JObject joe = new JObject
+            {
+                ["jsonrpc"] = "1.0",
+                ["id"] = "1",
+                ["method"] = method
+            };
+
+            if (parameters?.Length > 0)
+            {
+                JArray props = new JArray();
+                foreach (var p in parameters)
+                {
+                    props.Add(p);
+                }
+                joe.Add(new JProperty("params", props));
+            }
+
+            string s = JsonConvert.SerializeObject(joe);
+            byte[] byteArray = Encoding.UTF8.GetBytes(s);
+            webRequest.ContentLength = byteArray.Length;
+            
+            using (Stream dataStream = webRequest.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+            using (var webResponse = webRequest.GetResponse())
+            {
+                using (Stream str = webResponse.GetResponseStream())
+                {
+                    using (StreamReader sr = new StreamReader(str))
+                    {
+                        return JsonConvert.DeserializeObject<JObject>(sr.ReadToEnd());
+                    }
+                }
+            }
         }
 
-        public IActionResult Contact()
+        public IActionResult Balance()
         {
-            ViewData["Message"] = "Your contact page.";
+            var balance = InvokeMethod("getbalance");
 
-            return View();
+            return View(balance);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Address()
         {
-            return View();
+            var address = InvokeMethod("getnewaddress");
+            return View(address);
+        }
+
+        [HttpPost]
+        public IActionResult SendToAddress(string address, double amount)
+        {
+            var balance = InvokeMethod("getbalance");
+            var max = double.Parse(balance["result"].ToString());
+            if (!(amount > 0) || amount > max)
+            {
+                throw new Exception("Insufficient funds");
+            }
+            var transaction = InvokeMethod("sendtoaddress", address, amount);
+            return View("Transaction", transaction);
+        }
+
+        public IActionResult Unspent()
+        {
+            var unspent = InvokeMethod("listunspent");
+            return View(unspent);
+        }
+
+        [HttpPost]
+        public IActionResult Generate(int blocks)
+        {
+            var generate = InvokeMethod("generate", blocks);
+            return View(generate);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Bitcoined/Models/RootObject.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bitcoined.Models
+{
+    public class RootObject
+    {
+        [JsonProperty("txid")]
+        public string Txid { get; set; }
+
+        [JsonProperty("vout")]
+        public long Vout { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("redeemScript", NullValueHandling = NullValueHandling.Ignore)]
+        public string RedeemScript { get; set; }
+
+        [JsonProperty("scriptPubKey")]
+        public string ScriptPubKey { get; set; }
+
+        [JsonProperty("amount")]
+        public double Amount { get; set; }
+
+        [JsonProperty("confirmations")]
+        public long Confirmations { get; set; }
+
+        [JsonProperty("spendable")]
+        public bool Spendable { get; set; }
+
+        [JsonProperty("solvable")]
+        public bool Solvable { get; set; }
+
+        [JsonProperty("safe")]
+        public bool Safe
+        {
+            get; set;
+        }
+    }
+}
